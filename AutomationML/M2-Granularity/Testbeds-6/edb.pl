d2bd5a59bc77a730ef96269cba6515c66e2666d0
Irlan <irlangrangel2003@gmail.com>
2016-07-13 17:07:56 +0200

    tb 6

---
+++ AutomationML/M2-Granularity/Testbeds-6/edb.pl
@@ -0,0 +1,129 @@
+clause1(hasAttribute(systemUnitClass31,attribute91),true).
+clause1(hasAttribute(systemUnitClass31,attribute81),true).
+clause1(hasAttribute(systemUnitClass31,attribute71),true).
+clause1(hasAttributeName(systemUnitClass31,'BASIC_27-44-01-92 Industry connector (accessories)'),true).
+clause1(type(systemUnitClass31,systemUnitClass),true).
+clause1(hasSystemUnitClassLib(cAEXFile11,systemUnitClassLib11),true).
+clause1(hasExternalReference(cAEXFile11,externalReference21),true).
+clause1(hasExternalReference(cAEXFile11,externalReference11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Granularity-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(hasAttributeValue(attribute41,'9.0'),true).
+clause1(hasAttributeName(attribute41,'eClassVersion'),true).
+clause1(type(attribute41,attribute),true).
+clause1(refBaseClassPath(externalReference11,'Libs/RoleClass Libraries/AutomationMLBaseRoleClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference11,'BaseRoleClassLib'),true).
+clause1(type(externalReference11,externalReference),true).
+clause1(hasAttribute(systemUnitClass21,attribute61),true).
+clause1(hasAttribute(systemUnitClass21,attribute51),true).
+clause1(hasAttribute(systemUnitClass21,attribute41),true).
+clause1(hasAttributeName(systemUnitClass21,'BASIC_27-44-01-06 Sensor actuator adapter'),true).
+clause1(type(systemUnitClass21,systemUnitClass),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#05-AFR414#002'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasAttributeValue(attribute91,'0173-1---BASIC_1_1#92-AFR426#002'),true).
+clause1(hasAttributeName(attribute91,'eClassIRDI'),true).
+clause1(type(attribute91,attribute),true).
+clause1(hasAttribute(systemUnitClass11,attribute31),true).
+clause1(hasAttribute(systemUnitClass11,attribute21),true).
+clause1(hasAttribute(systemUnitClass11,attribute11),true).
+clause1(hasAttributeName(systemUnitClass11,'BASIC_27-44-01-05 Valve connector'),true).
+clause1(type(systemUnitClass11,systemUnitClass),true).
+clause1(hasAttributeValue(attribute21,'27440105'),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasAttributeValue(attribute81,'27440192'),true).
+clause1(hasAttributeName(attribute81,'eClassClassificationClass'),true).
+clause1(type(attribute81,attribute),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasAttributeValue(attribute71,'9.0'),true).
+clause1(hasAttributeName(attribute71,'eClassVersion'),true).
+clause1(type(attribute71,attribute),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-02-20'),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasAttributeValue(attribute61,'0173-1---BASIC_1_1#06-AFR417#002'),true).
+clause1(hasAttributeName(attribute61,'eClassIRDI'),true).
+clause1(type(attribute61,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+clause1(hasAttributeValue(attribute51,'27440106'),true).
+clause1(hasAttributeName(attribute51,'eClassClassificationClass'),true).
+clause1(type(attribute51,attribute),true).
+clause1(refBaseClassPath(externalReference21,'Libs/InterfaceClass Libraries/AutomationMLInterfaceClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference21,'BaseInterfaceClassLib'),true).
+clause1(type(externalReference21,externalReference),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass31),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass21),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass11),true).
+clause1(hasVersion(systemUnitClassLib11,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib11,'Industry Connector'),true).
+clause1(type(systemUnitClassLib11,systemUnitClassLib),true).
+
+clause1(refBaseClassPath(externalReference12,'Libs/RoleClass Libraries/AutomationMLBaseRoleClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference12,'BaseRoleClassLib'),true).
+clause1(type(externalReference12,externalReference),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#05-AFR414#002'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasAttributeValue(attribute62,'0173-1---BASIC_1_1#92-AFR426#002'),true).
+clause1(hasAttributeName(attribute62,'eClassIRDI'),true).
+clause1(type(attribute62,attribute),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasSystemUnitClassLib(cAEXFile12,systemUnitClassLib12),true).
+clause1(hasExternalReference(cAEXFile12,externalReference22),true).
+clause1(hasExternalReference(cAEXFile12,externalReference12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Granularity-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass22),true).
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass12),true).
+clause1(hasAttributeName(systemUnitClassLib12,'Industry Connector'),true).
+clause1(type(systemUnitClassLib12,systemUnitClassLib),true).
+clause1(refBaseClassPath(externalReference22,'Libs/InterfaceClass Libraries/AutomationMLInterfaceClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference22,'BaseInterfaceClassLib'),true).
+clause1(type(externalReference22,externalReference),true).
+clause1(hasAttribute(systemUnitClass12,attribute32),true).
+clause1(hasAttribute(systemUnitClass12,attribute22),true).
+clause1(hasAttribute(systemUnitClass12,attribute12),true).
+clause1(hasAttributeName(systemUnitClass12,'BASIC_27-44-01-05 Valve connector'),true).
+clause1(type(systemUnitClass12,systemUnitClass),true).
+clause1(hasAttributeValue(attribute42,'9.0'),true).
+clause1(hasAttributeName(attribute42,'eClassVersion'),true).
+clause1(type(attribute42,attribute),true).
+clause1(hasAttributeValue(attribute22,'27440105'),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasAttribute(systemUnitClass22,attribute62),true).
+clause1(hasAttribute(systemUnitClass22,attribute52),true).
+clause1(hasAttribute(systemUnitClass22,attribute42),true).
+clause1(hasAttributeName(systemUnitClass22,'BASIC_27-44-01-92 Industry connector (accessories)'),true).
+clause1(type(systemUnitClass22,systemUnitClass),true).
+clause1(hasAttributeValue(attribute52,'27440192'),true).
+clause1(hasAttributeName(attribute52,'eClassClassificationClass'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+
