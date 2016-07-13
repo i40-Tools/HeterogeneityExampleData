ae1c4248acb72153c902c3f82ab260cce7a2e8ec
Irlan <irlangrangel2003@gmail.com>
2016-07-13 17:01:48 +0200

    tb 4

---
+++ AutomationML/M2-Granularity/Testbeds-4/edb.pl
@@ -0,0 +1,121 @@
+clause1(refBaseClassPath(externalReference11,'Libs\\RoleClass Libraries\\AutomationMLBaseRoleClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference11,'AutomationMLBaseRoleClassLib'),true).
+clause1(type(externalReference11,externalReference),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#01-AKE197#013'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasAttributeValue(attribute81,'27061805'),true).
+clause1(hasAttributeName(attribute81,'eClassClassificationClass'),true).
+clause1(type(attribute81,attribute),true).
+clause1(hasAttribute(roleClass31,attribute91),true).
+clause1(hasAttribute(roleClass31,attribute81),true).
+clause1(hasAttribute(roleClass31,attribute71),true).
+clause1(hasAttributeName(roleClass31,'BASIC_27-06-18-05 Telecommunications cable'),true).
+clause1(type(roleClass31,roleClass),true).
+clause1(hasAttributeValue(attribute61,'0173-1---BASIC_1_1#02-AFZ688#015'),true).
+clause1(hasAttributeName(attribute61,'eClassIRDI'),true).
+clause1(type(attribute61,attribute),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasRoleClassLib(cAEXFile11,roleClassLib11),true).
+clause1(hasExternalReference(cAEXFile11,externalReference11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Granularity-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-02-20 '),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasRoleClass(roleClassLib11,roleClass31),true).
+clause1(hasRoleClass(roleClassLib11,roleClass21),true).
+clause1(hasRoleClass(roleClassLib11,roleClass11),true).
+clause1(hasVersion(roleClassLib11,'1.0'),true).
+clause1(hasAttributeName(roleClassLib11,'CommunicationCable'),true).
+clause1(type(roleClassLib11,roleClassLib),true).
+clause1(hasAttribute(roleClass11,attribute31),true).
+clause1(hasAttribute(roleClass11,attribute21),true).
+clause1(hasAttribute(roleClass11,attribute11),true).
+clause1(hasAttributeName(roleClass11,'BASIC_27-06-18-01 Data cable'),true).
+clause1(type(roleClass11,roleClass),true).
+clause1(hasAttributeValue(attribute41,'9.0'),true).
+clause1(hasAttributeName(attribute41,'eClassVersion'),true).
+clause1(type(attribute41,attribute),true).
+clause1(hasAttributeValue(attribute91,'0173-1---BASIC_1_1#05-AKE200#015'),true).
+clause1(hasAttributeName(attribute91,'eClassIRDI'),true).
+clause1(type(attribute91,attribute),true).
+clause1(hasAttributeValue(attribute21,'27061801 '),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasAttributeValue(attribute71,'9.0'),true).
+clause1(hasAttributeName(attribute71,'eClassVersion'),true).
+clause1(type(attribute71,attribute),true).
+clause1(hasAttribute(roleClass21,attribute61),true).
+clause1(hasAttribute(roleClass21,attribute51),true).
+clause1(hasAttribute(roleClass21,attribute41),true).
+clause1(hasAttributeName(roleClass21,'BASIC_27-06-18-02 Coaxial cable'),true).
+clause1(type(roleClass21,roleClass),true).
+clause1(hasAttributeValue(attribute51,'27061802'),true).
+clause1(hasAttributeName(attribute51,'eClassClassificationClass'),true).
+clause1(type(attribute51,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+
+clause1(refBaseClassPath(externalReference12,'Libs\\RoleClass Libraries\\AutomationMLBaseRoleClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference12,'AutomationMLBaseRoleClassLib'),true).
+clause1(type(externalReference12,externalReference),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#01-AKE197#013'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasAttributeValue(attribute62,'0173-1---BASIC_1_1#05-AKE200#015'),true).
+clause1(hasAttributeName(attribute62,'eClassIRDI'),true).
+clause1(type(attribute62,attribute),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasRoleClassLib(cAEXFile12,roleClassLib12),true).
+clause1(hasExternalReference(cAEXFile12,externalReference12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Granularity-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasRoleClass(roleClassLib12,roleClass22),true).
+clause1(hasRoleClass(roleClassLib12,roleClass12),true).
+clause1(hasAttributeName(roleClassLib12,'CommunicationCable'),true).
+clause1(type(roleClassLib12,roleClassLib),true).
+clause1(hasAttribute(roleClass12,attribute32),true).
+clause1(hasAttribute(roleClass12,attribute22),true).
+clause1(hasAttribute(roleClass12,attribute12),true).
+clause1(hasAttributeName(roleClass12,'BASIC_27-06-18-01 Data cable'),true).
+clause1(type(roleClass12,roleClass),true).
+clause1(hasAttributeValue(attribute42,'9.0'),true).
+clause1(hasAttributeName(attribute42,'eClassVersion'),true).
+clause1(type(attribute42,attribute),true).
+clause1(hasAttributeValue(attribute22,'27061801 '),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasAttribute(roleClass22,attribute62),true).
+clause1(hasAttribute(roleClass22,attribute52),true).
+clause1(hasAttribute(roleClass22,attribute42),true).
+clause1(hasAttributeName(roleClass22,'BASIC_27-06-18-05 Telecommunications cable'),true).
+clause1(type(roleClass22,roleClass),true).
+clause1(hasAttributeValue(attribute52,'27061805'),true).
+clause1(hasAttributeName(attribute52,'eClassClassificationClass'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+
