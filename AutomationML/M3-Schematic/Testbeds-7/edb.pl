5c59675355c9ee2725bc1584ea6955cca982ff40
Irlan <irlangrangel2003@gmail.com>
2016-07-14 16:26:03 +0200

    schematic test bed 7

---
+++ AutomationML/M3-Schematic/Testbeds-7/edb.pl
@@ -0,0 +1,300 @@
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass11),true).
+clause1(hasVersion(systemUnitClassLib11,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib11,'Conveyor'),true).
+clause1(type(systemUnitClassLib11,systemUnitClassLib),true).
+clause1(hasSystemUnitClassLib(cAEXFile11,systemUnitClassLib11),true).
+clause1(hasRoleClassLib(cAEXFile11,roleClassLib11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Schematic-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(type(roleRequirements11,roleRequirements),true).
+clause1(hasAttributeValue(attribute41,'9.0'),true).
+clause1(hasAttributeName(attribute41,'eClassVersion'),true).
+clause1(type(attribute41,attribute),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#11-AFX038#001'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic21,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic21,refSemantic),true).
+clause1(hasExternalInterface(systemUnitClass11,externalInterface21),true).
+clause1(hasAttribute(systemUnitClass11,attribute71),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements31),true).
+clause1(type(systemUnitClass11,systemUnitClass),true).
+clause1(hasAttribute(systemUnitClass11,attribute81),true).
+clause1(hasAttributeName(systemUnitClass11,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass11,attribute41),true).
+clause1(hasAttribute(systemUnitClass11,attribute51),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements21),true).
+clause1(hasAttribute(systemUnitClass11,attribute61),true).
+clause1(hasExternalInterface(systemUnitClass11,externalInterface11),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements11),true).
+clause1(hasAttributeValue(attribute21,'19200611'),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasRefSemantic(attribute81,refSemantic21),true).
+clause1(type(attribute81,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic11,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic11,refSemantic),true).
+clause1(type(externalInterface21,externalInterface),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasRefSemantic(attribute71,refSemantic11),true).
+clause1(type(attribute71,attribute),true).
+clause1(type(externalInterface11,externalInterface),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(roleRequirements31,roleRequirements),true).
+clause1(hasAttributeValue(attribute61,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute61,'eClassIRDI'),true).
+clause1(type(attribute61,attribute),true).
+clause1(hasAttribute(roleClass11,attribute31),true).
+clause1(hasAttribute(roleClass11,attribute21),true).
+clause1(hasAttribute(roleClass11,attribute11),true).
+clause1(hasAttributeName(roleClass11,'BASIC_19-20-06-11 Connection technology (Industry microcomputer)'),true).
+clause1(type(roleClass11,roleClass),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+clause1(type(roleRequirements21,roleRequirements),true).
+clause1(hasAttributeValue(attribute51,'36120401'),true).
+clause1(hasAttributeName(attribute51,'eClassClassificationClass'),true).
+clause1(type(attribute51,attribute),true).
+clause1(hasRoleClass(roleClassLib11,roleClass11),true).
+clause1(hasVersion(roleClassLib11,'1.0'),true).
+clause1(hasAttributeName(roleClassLib11,'Industry microcomputer'),true).
+clause1(type(roleClassLib11,roleClassLib),true).
+
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass12),true).
+clause1(hasVersion(systemUnitClassLib12,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib12,'Conveyor'),true).
+clause1(type(systemUnitClassLib12,systemUnitClassLib),true).
+clause1(hasSystemUnitClassLib(cAEXFile12,systemUnitClassLib12),true).
+clause1(hasRoleClassLib(cAEXFile12,roleClassLib12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Schematic-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(type(roleRequirements12,roleRequirements),true).
+clause1(hasAttributeValue(attribute42,'9.0'),true).
+clause1(hasAttributeName(attribute42,'eClassVersion'),true).
+clause1(type(attribute42,attribute),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#11-AFX038#001'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic22,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic22,refSemantic),true).
+clause1(hasExternalInterface(systemUnitClass12,externalInterface22),true).
+clause1(hasAttribute(systemUnitClass12,attribute72),true).
+clause1(type(systemUnitClass12,systemUnitClass),true).
+clause1(hasAttribute(systemUnitClass12,attribute82),true).
+clause1(hasAttributeName(systemUnitClass12,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass12,attribute42),true).
+clause1(hasAttribute(systemUnitClass12,attribute52),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements22),true).
+clause1(hasAttribute(systemUnitClass12,attribute62),true).
+clause1(hasExternalInterface(systemUnitClass12,externalInterface12),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements12),true).
+clause1(hasAttributeValue(attribute22,'19200611'),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasRefSemantic(attribute82,refSemantic22),true).
+clause1(hasAttributeValue(attribute82,'2'),true).
+clause1(type(attribute82,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic12,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic12,refSemantic),true).
+clause1(type(externalInterface22,externalInterface),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasRefSemantic(attribute72,refSemantic12),true).
+clause1(hasAttributeValue(attribute72,'1.5'),true).
+clause1(type(attribute72,attribute),true).
+clause1(type(externalInterface12,externalInterface),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass11),true).
+clause1(hasVersion(systemUnitClassLib11,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib11,'Conveyor'),true).
+clause1(type(systemUnitClassLib11,systemUnitClassLib),true).
+clause1(hasSystemUnitClassLib(cAEXFile11,systemUnitClassLib11),true).
+clause1(hasRoleClassLib(cAEXFile11,roleClassLib11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Schematic-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(type(roleRequirements11,roleRequirements),true).
+clause1(hasAttributeValue(attribute41,'9.0'),true).
+clause1(hasAttributeName(attribute41,'eClassVersion'),true).
+clause1(type(attribute41,attribute),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#11-AFX038#001'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic21,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic21,refSemantic),true).
+clause1(hasExternalInterface(systemUnitClass11,externalInterface21),true).
+clause1(hasAttribute(systemUnitClass11,attribute71),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements31),true).
+clause1(type(systemUnitClass11,systemUnitClass),true).
+clause1(hasAttribute(systemUnitClass11,attribute81),true).
+clause1(hasAttributeName(systemUnitClass11,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass11,attribute41),true).
+clause1(hasAttribute(systemUnitClass11,attribute51),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements21),true).
+clause1(hasAttribute(systemUnitClass11,attribute61),true).
+clause1(hasExternalInterface(systemUnitClass11,externalInterface11),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements11),true).
+clause1(hasAttributeValue(attribute21,'19200611'),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasRefSemantic(attribute81,refSemantic21),true).
+clause1(type(attribute81,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic11,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic11,refSemantic),true).
+clause1(type(externalInterface21,externalInterface),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasRefSemantic(attribute71,refSemantic11),true).
+clause1(type(attribute71,attribute),true).
+clause1(type(externalInterface11,externalInterface),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(roleRequirements31,roleRequirements),true).
+clause1(hasAttributeValue(attribute61,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute61,'eClassIRDI'),true).
+clause1(type(attribute61,attribute),true).
+clause1(hasAttribute(roleClass11,attribute31),true).
+clause1(hasAttribute(roleClass11,attribute21),true).
+clause1(hasAttribute(roleClass11,attribute11),true).
+clause1(hasAttributeName(roleClass11,'BASIC_19-20-06-11 Connection technology (Industry microcomputer)'),true).
+clause1(type(roleClass11,roleClass),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+clause1(type(roleRequirements21,roleRequirements),true).
+clause1(hasAttributeValue(attribute51,'36120401'),true).
+clause1(hasAttributeName(attribute51,'eClassClassificationClass'),true).
+clause1(type(attribute51,attribute),true).
+clause1(hasRoleClass(roleClassLib11,roleClass11),true).
+clause1(hasVersion(roleClassLib11,'1.0'),true).
+clause1(hasAttributeName(roleClassLib11,'Industry microcomputer'),true).
+clause1(type(roleClassLib11,roleClassLib),true).
+
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass12),true).
+clause1(hasVersion(systemUnitClassLib12,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib12,'Conveyor'),true).
+clause1(type(systemUnitClassLib12,systemUnitClassLib),true).
+clause1(hasSystemUnitClassLib(cAEXFile12,systemUnitClassLib12),true).
+clause1(hasRoleClassLib(cAEXFile12,roleClassLib12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Schematic-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(type(roleRequirements12,roleRequirements),true).
+clause1(hasAttributeValue(attribute42,'9.0'),true).
+clause1(hasAttributeName(attribute42,'eClassVersion'),true).
+clause1(type(attribute42,attribute),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#11-AFX038#001'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic22,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic22,refSemantic),true).
+clause1(hasExternalInterface(systemUnitClass12,externalInterface22),true).
+clause1(hasAttribute(systemUnitClass12,attribute72),true).
+clause1(type(systemUnitClass12,systemUnitClass),true).
+clause1(hasAttribute(systemUnitClass12,attribute82),true).
+clause1(hasAttributeName(systemUnitClass12,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass12,attribute42),true).
+clause1(hasAttribute(systemUnitClass12,attribute52),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements22),true).
+clause1(hasAttribute(systemUnitClass12,attribute62),true).
+clause1(hasExternalInterface(systemUnitClass12,externalInterface12),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements12),true).
+clause1(hasAttributeValue(attribute22,'19200611'),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasRefSemantic(attribute82,refSemantic22),true).
+clause1(hasAttributeValue(attribute82,'2'),true).
+clause1(type(attribute82,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic12,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic12,refSemantic),true).
+clause1(type(externalInterface22,externalInterface),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasRefSemantic(attribute72,refSemantic12),true).
+clause1(hasAttributeValue(attribute72,'1.5'),true).
+clause1(type(attribute72,attribute),true).
+clause1(type(externalInterface12,externalInterface),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation22,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation22,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation22,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasAttributeValue(attribute62,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute62,'eClassIRDI'),true).
+clause1(type(attribute62,attribute),true).
+clause1(hasAttribute(roleClass12,attribute32),true).
+clause1(hasAttribute(roleClass12,attribute22),true).
+clause1(hasAttribute(roleClass12,attribute12),true).
+clause1(hasAttributeName(roleClass12,'BASIC_19-20-06-11 Connection technology (Industry microcomputer)'),true).
+clause1(type(roleClass12,roleClass),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+clause1(type(roleRequirements22,roleRequirements),true).
+clause1(hasAttributeValue(attribute52,'36120401'),true).
+clause1(hasAttributeName(attribute52,'eClassClassificationClass'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasRoleClass(roleClassLib12,roleClass12),true).
+clause1(hasVersion(roleClassLib12,'1.0'),true).
+clause1(hasAttributeName(roleClassLib12,'Industry microcomputer'),true).
+clause1(type(roleClassLib12,roleClassLib),true).
+clause1(hasWriterVersion(additionalInformation22,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation22,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasAttributeValue(attribute62,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute62,'eClassIRDI'),true).
+clause1(type(attribute62,attribute),true).
+clause1(hasAttribute(roleClass12,attribute32),true).
+clause1(hasAttribute(roleClass12,attribute22),true).
+clause1(hasAttribute(roleClass12,attribute12),true).
+clause1(hasAttributeName(roleClass12,'BASIC_19-20-06-11 Connection technology (Industry microcomputer)'),true).
+clause1(type(roleClass12,roleClass),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+clause1(type(roleRequirements22,roleRequirements),true).
+clause1(hasAttributeValue(attribute52,'36120401'),true).
+clause1(hasAttributeName(attribute52,'eClassClassificationClass'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasRoleClass(roleClassLib12,roleClass12),true).
+clause1(hasVersion(roleClassLib12,'1.0'),true).
+clause1(hasAttributeName(roleClassLib12,'Industry microcomputer'),true).
+clause1(type(roleClassLib12,roleClassLib),true).
+
--- AutomationML/M3-Schematic/Testbeds-7/readme.md
+++ AutomationML/M3-Schematic/Testbeds-7/readme.md
@@ -9,21 +9,38 @@ It contains the following conflicts:
 
 Representation: Pair(File a, File b)
 
+1: pair (SystemUnitClass("BASIC_36-12-04-01 Conveyor"),(SystemUnitClass("BASIC_36-12-04-01 Conveyor"))
 
-1: pair(Attribute("maxConveyingSpeed") , (Attribute("maxConveyingSpeed"))
+2: pair(Attribute(eClassVersion) , (Attribute(eClassVersion))
 
- - pair (RefSemantic,RefSemantic)
+3: pair(Attribute(eClassClassificationClass) , (Attribute(eClassClassificationClass))
 
-2: pair(Attribute("Max.load capacity") , (Attribute("Max.load capacity"))
+4: pair(Attribute(eClassIRDI) , (Attribute(eClassIRDI)
 
- - pair (RefSemantic,RefSemantic)
 
-3: pair(Attribute("Min. speed of the drive") , (Attribute("Min. speed of the drive"))
-
- - pair (RefSemantic,RefSemantic)
-
-4: pair(Attribute("maxTransportationWeight") , (Attribute("maxTransportationWeight"))
+5: pair(Attribute(maxConveyingSpeed) , (Attribute(maxConveyingSpeed))
 
  - pair (RefSemantic,RefSemantic)
 
+6: pair(Attribute("Max.load capacity") , (Attribute("Max.load capacity"))
 
+ - pair (RefSemantic,RefSemantic)
+
+
+7: pair(InternalElement("Conveyor1") ,(InternalElement("Conveyor1"))
+ 
+ - pair (RoleRequirement,RoleRequirement).
+
+
+8: pair(InternalElement("Conveyor2") ,(InternalElement("Conveyor2"))
+ 
+ - pair (RoleRequirement,RoleRequirement).
+
+
+9: pair(RoleClass) , pair(RoleClass))
+
+10: pair(Attribute(eClassVersion) , (Attribute(eClassVersion))
+
+11: pair(Attribute(eClassClassificationClass) , (Attribute(eClassClassificationClass))
+
+12: pair(Attribute(eClassIRDI) , (Attribute(eClassIRDI))  
