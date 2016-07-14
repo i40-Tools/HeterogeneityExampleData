6e3f5ed42e6deaa9e2de2a02bb366ad92dd5ad21
Irlan <irlangrangel2003@gmail.com>
2016-07-14 16:05:58 +0200

    schematic test bed 2

---
+++ AutomationML/M3-Schematic/Testbeds-2/edb.pl
@@ -0,0 +1,115 @@
+clause1(refBaseClassPath(externalReference11,'Libs/InterfaceClass Libraries/AutomationMLInterfaceClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference11,'BaseInterfaceClassLib'),true).
+clause1(type(externalReference11,externalReference),true).
+clause1(refBaseRoleClassPath(roleRequirements21,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Resource'),true).
+clause1(type(roleRequirements21,roleRequirements),true).
+clause1(refBaseRoleClassPath(roleRequirements51,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Resource'),true).
+clause1(type(roleRequirements51,roleRequirements),true).
+clause1(hasInternalElement(instanceHierarchy11,internalElement11),true).
+clause1(hasAttributeName(instanceHierarchy11,'ConveyorSystem'),true).
+clause1(type(instanceHierarchy11,instanceHierarchy),true).
+clause1(hasCorrespondingAttributePath(refSemantic21,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic21,refSemantic),true).
+clause1(type(externalInterface11,externalInterface),true).
+clause1(hasRefSemantic(attribute11,refSemantic11),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasInstanceHierarchy(cAEXFile11,instanceHierarchy11),true).
+clause1(hasExternalReference(cAEXFile11,externalReference11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Schematic-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasExternalInterface(internalElement11,externalInterface21),true).
+clause1(identifier(internalElement11,'acf9f51b-3fbd-4f30-891d-f8daca50ed17'),true).
+clause1(type(internalElement11,internalElement),true).
+clause1(hasRoleRequirements(internalElement11,roleRequirements21),true).
+clause1(hasAttributeName(internalElement11,'Connection'),true).
+clause1(hasRoleRequirements(internalElement11,roleRequirements31),true).
+clause1(hasAttribute(internalElement11,attribute11),true).
+clause1(hasRoleRequirements(internalElement11,roleRequirements41),true).
+clause1(hasAttribute(internalElement11,attribute21),true).
+clause1(hasRoleRequirements(internalElement11,roleRequirements11),true).
+clause1(hasExternalInterface(internalElement11,externalInterface11),true).
+clause1(hasRoleRequirements(internalElement11,roleRequirements51),true).
+clause1(type(roleRequirements31,roleRequirements),true).
+clause1(type(externalInterface21,externalInterface),true).
+clause1(hasRefSemantic(attribute21,refSemantic21),true).
+clause1(type(attribute21,attribute),true).
+clause1(type(roleRequirements11,roleRequirements),true).
+clause1(hasCorrespondingAttributePath(refSemantic11,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic11,refSemantic),true).
+clause1(refBaseRoleClassPath(roleRequirements41,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Port'),true).
+clause1(type(roleRequirements41,roleRequirements),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+
+clause1(refBaseClassPath(externalReference12,'Libs/InterfaceClass Libraries/AutomationMLInterfaceClassLib.aml'),true).
+clause1(externalReferenceAlias(externalReference12,'BaseInterfaceClassLib'),true).
+clause1(type(externalReference12,externalReference),true).
+clause1(refBaseRoleClassPath(roleRequirements22,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Resource'),true).
+clause1(type(roleRequirements22,roleRequirements),true).
+clause1(hasCorrespondingAttributePath(refSemantic22,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic22,refSemantic),true).
+clause1(hasInternalElement(instanceHierarchy12,internalElement32),true).
+clause1(hasInternalElement(instanceHierarchy12,internalElement12),true).
+clause1(hasAttributeName(instanceHierarchy12,'ConveyorSystem'),true).
+clause1(type(instanceHierarchy12,instanceHierarchy),true).
+clause1(hasRoleRequirements(internalElement32,roleRequirements42),true).
+clause1(hasRoleRequirements(internalElement32,roleRequirements32),true).
+clause1(hasExternalInterface(internalElement32,externalInterface22),true).
+clause1(hasAttribute(internalElement32,attribute22),true).
+clause1(hasAttributeName(internalElement32,'Conveyor2'),true).
+clause1(identifier(internalElement32,'11b59bf6-44f6-4650-939f-d8e2551912a7'),true).
+clause1(type(internalElement32,internalElement),true).
+clause1(type(externalInterface12,externalInterface),true).
+clause1(hasRefSemantic(attribute12,refSemantic12),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasInstanceHierarchy(cAEXFile12,instanceHierarchy12),true).
+clause1(hasExternalReference(cAEXFile12,externalReference12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Schematic-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation22,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation22,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation22,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasRoleRequirements(internalElement12,roleRequirements22),true).
+clause1(hasRoleRequirements(internalElement12,roleRequirements12),true).
+clause1(hasExternalInterface(internalElement12,externalInterface12),true).
+clause1(hasAttribute(internalElement12,attribute12),true).
+clause1(hasAttributeName(internalElement12,'Conveyor1'),true).
+clause1(identifier(internalElement12,'45e20f8e-7351-4af9-bdfd-3c019674de55'),true).
+clause1(type(internalElement12,internalElement),true).
+clause1(refBaseRoleClassPath(roleRequirements32,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Port'),true).
+clause1(type(roleRequirements32,roleRequirements),true).
+clause1(type(externalInterface22,externalInterface),true).
+clause1(hasRefSemantic(attribute22,refSemantic22),true).
+clause1(type(attribute22,attribute),true).
+clause1(refBaseRoleClassPath(roleRequirements12,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Port'),true).
+clause1(type(roleRequirements12,roleRequirements),true).
+clause1(hasCorrespondingAttributePath(refSemantic12,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic12,refSemantic),true).
+clause1(refBaseRoleClassPath(roleRequirements42,'AutomationMLBaseRoleClassLib/AutomationMLBaseRole/Port'),true).
+clause1(type(roleRequirements42,roleRequirements),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+
---
+++ AutomationML/M3-Schematic/Testbeds-2/evalAML.pl
@@ -0,0 +1,19 @@
+:- multifile(clause1/2).
+ eval:- 
+ assert(amlPredicates(
+ [
+ 'sameRoleClassLib','sameRoleClass','sameInterfaceClass','sameSystemUnitClass',
+ 'eClassClassificationAtt','eClassVersionAtt','eClassIRDIAtt','sameAttribute' 
+ ] 
+ )),    
+ consult('d:/Deutch/development/Rules4AMLIntegration/resources/files/semi1.pl'),
+ consult('d:/Deutch/development/Rules4AMLIntegration/resources/files/AML_rules.pl'),
+ consult('d:/Deutch/development/Rules4AMLIntegration/resources/files/edb.pl'),
+ %consult('d:/Deutch/development/Rules4AMLIntegration/resources/files/TestData.pl'),
+ tdb.       
+                  
+ writePredicates:-
+ open('d:/Deutch/development/Rules4AMLIntegration/resources/files/output.txt',write,FileName),
+ consult("d:/Deutch/development/Rules4AMLIntegration/resources/files/writeRules.pl"),
+ myWrite(FileName),close(FileName).          
+                                                              
