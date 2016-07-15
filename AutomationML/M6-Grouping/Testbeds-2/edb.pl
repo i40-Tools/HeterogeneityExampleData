2c78a986569466e649c26d96311e19403bc13c4a
Irlan <irlangrangel2003@gmail.com>
2016-07-15 11:46:42 +0200

    Updating groupping

---
+++ AutomationML/M6-Grouping/Testbeds-2/edb.pl
@@ -0,0 +1,220 @@
+clause1(type(roleRequirements21,roleRequirements),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic21,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic21,refSemantic),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasSystemUnitClassLib(cAEXFile11,systemUnitClassLib11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Grouping-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass11),true).
+clause1(hasVersion(systemUnitClassLib11,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib11,'Conveyor'),true).
+clause1(type(systemUnitClassLib11,systemUnitClassLib),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements31),true).
+clause1(type(systemUnitClass11,systemUnitClass),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements41),true).
+clause1(hasAttributeName(systemUnitClass11,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass11,attribute31),true).
+clause1(hasAttribute(systemUnitClass11,attribute41),true).
+clause1(hasAttribute(systemUnitClass11,attribute51),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements21),true).
+clause1(hasAttribute(systemUnitClass11,attribute11),true).
+clause1(hasAttribute(systemUnitClass11,attribute21),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements11),true).
+clause1(type(roleRequirements31,roleRequirements),true).
+clause1(hasRefSemantic(attribute41,refSemantic11),true).
+clause1(hasAttributeValue(attribute41,'10'),true).
+clause1(type(attribute41,attribute),true).
+clause1(type(roleRequirements11,roleRequirements),true).
+clause1(hasAttributeValue(attribute21,'36120401'),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic11,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic11,refSemantic),true).
+clause1(type(roleRequirements41,roleRequirements),true).
+clause1(hasRefSemantic(attribute51,refSemantic21),true).
+clause1(hasAttributeValue(attribute51,'2'),true).
+clause1(type(attribute51,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+
+clause1(type(roleRequirements22,roleRequirements),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic22,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic22,refSemantic),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasSystemUnitClassLib(cAEXFile12,systemUnitClassLib12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Grouping-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation22,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation22,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation22,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass12),true).
+clause1(hasVersion(systemUnitClassLib12,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib12,'Conveyor'),true).
+clause1(type(systemUnitClassLib12,systemUnitClassLib),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements22),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements12),true).
+clause1(hasAttribute(systemUnitClass12,attribute52),true).
+clause1(hasAttribute(systemUnitClass12,attribute42),true).
+clause1(hasAttribute(systemUnitClass12,attribute32),true).
+clause1(hasAttribute(systemUnitClass12,attribute22),true).
+clause1(hasAttribute(systemUnitClass12,attribute12),true).
+clause1(hasAttributeName(systemUnitClass12,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(type(systemUnitClass12,systemUnitClass),true).
+clause1(hasRefSemantic(attribute42,refSemantic12),true).
+clause1(hasAttributeValue(attribute42,'10'),true).
+clause1(type(attribute42,attribute),true).
+clause1(type(roleRequirements12,roleRequirements),true).
+clause1(hasAttributeValue(attribute22,'36120401'),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic12,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic12,refSemantic),true).
+clause1(hasRefSemantic(attribute52,refSemantic22),true).
+clause1(hasAttributeValue(attribute52,'2'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+
+clause1(type(roleRequirements21,roleRequirements),true).
+clause1(hasAttributeValue(attribute31,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute31,'eClassIRDI'),true).
+clause1(type(attribute31,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic21,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic21,refSemantic),true).
+clause1(hasAttributeValue(attribute11,'9.0'),true).
+clause1(hasAttributeName(attribute11,'eClassVersion'),true).
+clause1(type(attribute11,attribute),true).
+clause1(hasSystemUnitClassLib(cAEXFile11,systemUnitClassLib11),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation21),true).
+clause1(hasAdditionalInfomation(cAEXFile11,additionalInformation11),true).
+clause1(hasSchemaVersion(cAEXFile11,'2.15'),true).
+clause1(hasFileName(cAEXFile11,'seed-Grouping-0.aml'),true).
+clause1(type(cAEXFile11,cAEXFile),true).
+clause1(hasWriterID(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation21,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation21,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation21,'AutomationML e.V.'),true).
+clause1(type(additionalInformation21,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation21,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation21,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation21,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation21,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation21,'AutomationML e.V.'),true).
+clause1(hasSystemUnitClass(systemUnitClassLib11,systemUnitClass11),true).
+clause1(hasVersion(systemUnitClassLib11,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib11,'Conveyor'),true).
+clause1(type(systemUnitClassLib11,systemUnitClassLib),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements31),true).
+clause1(type(systemUnitClass11,systemUnitClass),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements41),true).
+clause1(hasAttributeName(systemUnitClass11,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(hasAttribute(systemUnitClass11,attribute31),true).
+clause1(hasAttribute(systemUnitClass11,attribute41),true).
+clause1(hasAttribute(systemUnitClass11,attribute51),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements21),true).
+clause1(hasAttribute(systemUnitClass11,attribute11),true).
+clause1(hasAttribute(systemUnitClass11,attribute21),true).
+clause1(hasRoleRequirements(systemUnitClass11,roleRequirements11),true).
+clause1(type(roleRequirements31,roleRequirements),true).
+clause1(hasRefSemantic(attribute41,refSemantic11),true).
+clause1(hasAttributeValue(attribute41,'10'),true).
+clause1(type(attribute41,attribute),true).
+clause1(type(roleRequirements11,roleRequirements),true).
+clause1(hasAttributeValue(attribute21,'36120401'),true).
+clause1(hasAttributeName(attribute21,'eClassClassificationClass'),true).
+clause1(type(attribute21,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic11,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic11,refSemantic),true).
+clause1(type(roleRequirements41,roleRequirements),true).
+clause1(hasRefSemantic(attribute51,refSemantic21),true).
+clause1(hasAttributeValue(attribute51,'2'),true).
+clause1(type(attribute51,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation11,'2.0'),true).
+clause1(type(additionalInformation11,additionalInformation),true).
+
+clause1(type(roleRequirements22,roleRequirements),true).
+clause1(hasAttributeValue(attribute32,'0173-1---BASIC_1_1#01-AKL503#010'),true).
+clause1(hasAttributeName(attribute32,'eClassIRDI'),true).
+clause1(type(attribute32,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic22,'ECLASS:0173-1#02-BAF316#002'),true).
+clause1(type(refSemantic22,refSemantic),true).
+clause1(hasAttributeValue(attribute12,'9.0'),true).
+clause1(hasAttributeName(attribute12,'eClassVersion'),true).
+clause1(type(attribute12,attribute),true).
+clause1(hasSystemUnitClassLib(cAEXFile12,systemUnitClassLib12),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation22),true).
+clause1(hasAdditionalInfomation(cAEXFile12,additionalInformation12),true).
+clause1(hasSchemaVersion(cAEXFile12,'2.15'),true).
+clause1(hasFileName(cAEXFile12,'seed-Grouping-1.aml'),true).
+clause1(type(cAEXFile12,cAEXFile),true).
+clause1(hasWriterID(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasWriterRelease(additionalInformation22,'1.0'),true).
+clause1(hasWriterVendorURL(additionalInformation22,'www.AutomationML.org'),true).
+clause1(hasWriterName(additionalInformation22,'AutomationML e.V.'),true).
+clause1(type(additionalInformation22,additionalInformation),true).
+clause1(hasWriterProjectTitle(additionalInformation22,'AutomationML Tutorial Examples'),true).
+clause1(hasLastWritingDateTime(additionalInformation22,'2012-03-27 '),true).
+clause1(hasWriterVersion(additionalInformation22,'1.0'),true).
+clause1(hasWriterProjectID(additionalInformation22,'AutomationML Tutorial Examples '),true).
+clause1(hasWriterVendor(additionalInformation22,'AutomationML e.V.'),true).
+clause1(hasSystemUnitClass(systemUnitClassLib12,systemUnitClass12),true).
+clause1(hasVersion(systemUnitClassLib12,'1.0'),true).
+clause1(hasAttributeName(systemUnitClassLib12,'Conveyor'),true).
+clause1(type(systemUnitClassLib12,systemUnitClassLib),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements22),true).
+clause1(hasRoleRequirements(systemUnitClass12,roleRequirements12),true).
+clause1(hasAttribute(systemUnitClass12,attribute52),true).
+clause1(hasAttribute(systemUnitClass12,attribute42),true).
+clause1(hasAttribute(systemUnitClass12,attribute32),true).
+clause1(hasAttribute(systemUnitClass12,attribute22),true).
+clause1(hasAttribute(systemUnitClass12,attribute12),true).
+clause1(hasAttributeName(systemUnitClass12,'BASIC_36-12-04-01 Conveyor'),true).
+clause1(type(systemUnitClass12,systemUnitClass),true).
+clause1(hasRefSemantic(attribute42,refSemantic12),true).
+clause1(hasAttributeValue(attribute42,'10'),true).
+clause1(type(attribute42,attribute),true).
+clause1(type(roleRequirements12,roleRequirements),true).
+clause1(hasAttributeValue(attribute22,'36120401'),true).
+clause1(hasAttributeName(attribute22,'eClassClassificationClass'),true).
+clause1(type(attribute22,attribute),true).
+clause1(hasCorrespondingAttributePath(refSemantic12,'ECLASS:0173-1#02-BAF685#003'),true).
+clause1(type(refSemantic12,refSemantic),true).
+clause1(hasRefSemantic(attribute52,refSemantic22),true).
+clause1(hasAttributeValue(attribute52,'2'),true).
+clause1(type(attribute52,attribute),true).
+clause1(hasAutomationMLVersion(additionalInformation12,'2.0'),true).
+clause1(type(additionalInformation12,additionalInformation),true).
+
